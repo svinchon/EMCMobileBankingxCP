b2ffbbdf90c21a03d1140501c19d5ff2020425b0
svinchon <svinchon@gmail.com>
2015-10-01 18:06:21 +0200

    new code to guide while taking picture

---
+++ app/src/main/java/ing/rbi/poc/SVQuadView.java
@@ -0,0 +1,64 @@
+/** -------------------------------------------------------------------------
+ * Copyright 2013-2015 EMC Corporation.  All rights reserved.
+ ---------------------------------------------------------------------------- */
+
+package ing.rbi.poc;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PointF;
+import android.view.View;
+
+/**
+ * This class draws a quad overlay
+ */
+public class SVQuadView extends View {
+
+    private Path _path = new Path();
+    private Paint _paint = new Paint();
+    private PointF[] _quadPoints;       // The view's quad points
+    private boolean _valid;             // true for good quality
+
+    public SVQuadView(Context context) {
+        super(context);
+
+        _paint.setStyle(Paint.Style.STROKE);
+        _paint.setAntiAlias(true);
+        _paint.setStrokeWidth(5);
+    }
+
+    void setQuadCorners(PointF[] quadPoints, boolean valid)
+    {
+        setVisibility(View.VISIBLE);
+        _quadPoints = quadPoints;
+        _valid = valid;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        if (_quadPoints != null) {
+            _path.reset();
+
+            // top left
+            _path.moveTo(_quadPoints[0].x, _quadPoints[0].y);
+            // top right
+            _path.lineTo(_quadPoints[1].x, _quadPoints[1].y);
+            // bottom right
+            _path.lineTo(_quadPoints[2].x, _quadPoints[2].y);
+            // bottom left
+            _path.lineTo(_quadPoints[3].x, _quadPoints[3].y);
+            // back to top left to make it complete
+            _path.lineTo(_quadPoints[0].x, _quadPoints[0].y);
+            _path.close();
+
+            _paint.setColor(_valid ? Color.GREEN: Color.RED);
+            canvas.drawPath(_path, _paint);
+        }
+    }
+}
