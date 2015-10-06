d58b1b0f3c5e973aedb4e7023677c5061a3a858f
svinchon <svinchon@gmail.com>
2015-10-06 17:49:20 +0200

    cleaning

--- app/src/main/java/ing/rbi/poc/ProofIDHandler.java
+++ app/src/main/java/ing/rbi/poc/ProofIDHandler.java
@@ -227,8 +227,8 @@ public class ProofIDHandler implements MediaSelectedHandler{
         String forename = PIDResults.Forename;
         String surname = PIDResults.Surname;
 
-        EditText addressText = (EditText) proof_details_view.findViewById(ing.rbi.poc.R.id.txt_address);
-        addressText.setText(address);
+        //EditText addressText = (EditText) proof_details_view.findViewById(ing.rbi.poc.R.id.txt_address);
+        //addressText.setText(address);
 
         EditText dOBText = (EditText) proof_details_view.findViewById(ing.rbi.poc.R.id.txt_Invoice_Amount);
         dOBText.setText(dOB);
